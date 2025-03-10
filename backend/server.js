f04f516cff79417ab00870046bb827cb1f93b981
wendsaw <sawadogo.edgard@gmail.com>
2025-03-10 14:15:55 -0600

    @latest

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+mongoose.connect(process.env.MONGO_URL) 
+  .then(() => {
+    app.listen(port, () => {
+      console.log("Listening on port 3000");
+      console.log('Database connected');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
-import express from 'express'
 
-const port =3000
+const port = 3000;
+const app = express();
 
-const app=express()
 
-app.listen(port)
+app.get('/', (req, res) => {
+  res.send('hello');
+});
 
-app.get('/',(req,res)=>{
+app.get('/jobs', (req, res) => {
+  res.json({ message: "welcome to the api" });
+});
 
-    res.send('hello')
-})
+
