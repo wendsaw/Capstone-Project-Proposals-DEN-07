f830992762aeb42d2707e0f9bed0ffb5b7a81ef0
wendsaw <sawadogo.edgard@gmail.com>
2025-03-10 10:35:43 -0600

    @latest

--- frontend/src/firebase/Confi.js
+++ frontend/src/firebase/Confi.js
@@ -6,7 +6,7 @@ import 'firebase/auth'
 
 
 
-const firebaseConfig =  {
+const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
