283576a739f169bc472c42786a65150f9ba2e600
wendsaw <sawadogo.edgard@gmail.com>
2025-03-17 22:33:35 -0600

    @latest

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -23,6 +23,21 @@ module.exports.contact_user= async (req,res)=>{
         res.status(400).json(err)
     }
 }
+module.exports.contact_all= async (req,res)=>{
+
+    try {
+        const cont= await Contact.find({})
+        console.log(cont)
+        res.status(201).json(cont)
+        console.log(' contact created');
+    } catch(err) {
+
+        console.log('error');
+        
+        
+        res.status(400).json(err)
+    }
+}
 
 
 
--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -6,12 +6,13 @@ const express = require('express')
 
 // controller functions
 
-const {contact_user}=require('../controllers/contactController')
+const {contact_user, contact_all}=require('../controllers/contactController')
 const router = express.Router()
 
 // contact Route
 
 router.post('/contact', contact_user)
+router.get('/contact', contact_all)
 
 
 module.exports = router
